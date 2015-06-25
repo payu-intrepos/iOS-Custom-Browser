de89d62b8ca41b325d7816c7953617ef77cf2635
pointiitm <ankur@AnkurSinghvi.local>
2015-06-25 14:21:37 +0530

    Universal packages, Bug fixes

---
+++ iOSCustomBrowser/PayU_CB_SDK.h
@@ -0,0 +1,17 @@
+//
+//  PayU_CB_SDK.h
+//  iOSCustomeBrowser
+//
+//  Created by Suryakant Sharma on 20/04/15.
+//  Copyright (c) 2015 PayU, India. All rights reserved.
+//
+
+#ifndef iOSCustomeBrowser_PayU_CB_SDK_h
+#define iOSCustomeBrowser_PayU_CB_SDK_h
+
+#define CB_VERSION_NAME @"3.1"
+#define CB_VERSION_CODE 1
+#import "CBAllPaymentOption.h"
+#import "CBConnectionHandler.h"
+
+#endif
