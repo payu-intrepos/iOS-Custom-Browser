78218a3a2ad4409ee580702835682d736b6d033f
Shubham Garg <shubham.garg@payu.in>
2022-06-20 14:48:07 +0530

    Update Analytic Dependency in SPM

--- Package.swift
+++ Package.swift
@@ -10,9 +10,21 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PayUIndia-Custom-Browser",
-            targets: ["PayUCustomBrowser"]),
+            targets: ["PayUIndia-CustomBrowserTarget"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(name: "PayUIndia-Analytics",url: "https://github.com/payu-intrepos/PayUAnalytics-iOS", from: "3.0.0")
     ],
     targets: [
-        .binaryTarget(name: "PayUCustomBrowser", path: "./PayUCustomBrowser.xcframework")
+        .binaryTarget(name: "PayUCustomBrowser", path: "./PayUCustomBrowser.xcframework"),
+        .target(
+                name: "PayUIndia-CustomBrowserTarget",
+                dependencies: [
+                    "PayUCustomBrowser",
+                    "PayUIndia-Analytics"
+                ],
+                path: "/PayUIndia-CustomBrowserWrapper"
+            )
     ]
 )
